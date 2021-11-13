a7b4a87580616aa7e637fbe8d871abca4cf2a005
mvalhondo <mvalhondo96@gmail.com>
2021-11-13 19:11:56 +0100

    add validation for book delete

--- src/Persisters/BookPersister.php
+++ src/Persisters/BookPersister.php
@@ -11,4 +11,25 @@ class BookPersister extends BasePersister
     {
         $this->repo = $repo;
     }
+
+    /**
+     * Validates wheter data can be deleted
+     * 
+     * @param aray $data
+     * @return bool
+     */
+    protected function canBeDeleted(int $id): bool
+    {
+        $book = $this->repo->find($id);
+
+        if(is_null($book)) {
+            return false;
+        }
+
+        if(count($book->rents) > 0) {
+            return false;
+        }
+        
+        return true;
+    }
 }

74c4101325ff2b500b5c4f0b894ba0b42d06236e
mvalhondo <mvalhondo96@gmail.com>
2021-11-13 15:41:04 +0100

    add validation in user repo interface

--- src/Contracts/UserRepositoryInterface.php
+++ src/Contracts/UserRepositoryInterface.php
@@ -6,4 +6,11 @@ use Library\Contracts\BaseRepositoryInterface;
 
 interface UserRepositoryInterface extends BaseRepositoryInterface
 {
+    /**
+     * Finds the user by email
+     * 
+     * @param string $email
+     * @return object|null
+     */
+    public function findByEmail(string $email): object|null;
 }

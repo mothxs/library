5bbc01043e31dddf4eedf07cc3be8a7c14e6da14
mvalhondo <mvalhondo96@gmail.com>
2021-11-14 21:26:28 +0100

    update configure.sh file for easier initial deploy

--- configure.sh
+++ configure.sh
@@ -1,27 +1,33 @@
-#Install dependencies
-echo "Installing dependencies..."
-composer install
-echo "Dependencies installed!"
-
-#Configure environment
-echo "Configuring environment..."
-cp .env.example .env
-echo "Environment configured!"
-
-#Start docker containers as daemon
-echo "Starting docker containers..."
-vendor/bin/sail up -d
-echo "Docker containers started!"
-
 #Generate an encryption key for the app
 echo "Generating encription key"
 vendor/bin/sail artisan key:generate
 echo "Encription key generated!"
 
+#Publish assets
+echo "Publishing assets..."
+vendor/bin/sail artisan vendor:publish --tag=public
+vendor/bin/sail artisan vendor:publish --tag=resource
+echo "Published!"
+
+#Install npm dependencies
+echo "Installing npm dependencies..."
+vendor/bin/sail npm isntall
+echo "npm dependencies installed!"
+
+#Compile assets
+echo "Compiling assets..."
+vendor/bin/sail npm run dev
+echo "Assets compiled!"
+
 #Execute migrations
 echo "Executing migrations..."
 vendor/bin/sail artisan migrate
 echo "Migrations executed!"
+
+#Link storage
+echo "Linking storage..."
+vendor/bin/sail artisan storage:link
+echo "Storage linked!"
 
 #Creates a new user
 echo "Creating user..."

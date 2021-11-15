5bf3cbc3298497ee72ba51d74ace3049351be5c0
mvalhondo <mvalhondo96@gmail.com>
2021-11-15 22:44:03 +0100

    add favicon

--- src/resources/views/base/head.blade.php
+++ src/resources/views/base/head.blade.php
@@ -4,12 +4,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Biblioteca</title>
 
+    <link rel="icon" href="/img/favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital@1&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css">
-    
+
     <link rel="stylesheet" href="/css/app.css">
 </head>

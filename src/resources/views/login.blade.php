7c94411c77a908a83ecd987b444c8e14a2690fe0
mvalhondo <mvalhondo96@gmail.com>
2021-11-15 22:44:37 +0100

    add favicon to login

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+
+    <link rel="icon" href="/img/favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital@1&display=swap" rel="stylesheet">

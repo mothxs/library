f39f3e4ca5b0facbbd7c41f812bfa5cfd3d18172
mothxs <mvalhondo96@gmail.com>
2022-06-20 21:49:57 +0200

    make base controller abstract

--- src/Controllers/Api/BaseApiController.php
+++ src/Controllers/Api/BaseApiController.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class BaseApiController extends Controller
+abstract class BaseApiController extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
